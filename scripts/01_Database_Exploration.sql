21ea37e095724570db1756e420ec7b14233a2f41
SUMER AZAM <sumerazam916@gmail.com>
2026-07-27 22:49:10 +0530

    Update 01_Database_Exploration.sql

--- scripts/01_Database_Exploration.sql
+++ scripts/01_Database_Exploration.sql
@@ -1,7 +1,6 @@
+
+        --**************************************************** 1.DATABASE EXPLORATION ************************************************************
 /*
-===============================================================================
-Database Exploration
-===============================================================================
 Purpose:
     - To explore the structure of the database, including the list of tables and their schemas.
     - To inspect the columns and metadata for specific tables.

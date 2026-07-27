f9f72d61d4a974b6f7d8f6604208bcfd7726c9aa
SUMER AZAM <sumerazam916@gmail.com>
2026-07-27 22:46:16 +0530

    Update 01_Database_Exploration.sql

--- scripts/01_Database_Exploration.sql
+++ scripts/01_Database_Exploration.sql
@@ -35,3 +35,31 @@ SELECT
     CHARACTER_MAXIMUM_LENGTH
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'dim_products';
+
+
+select * from INFORMATION_SCHEMA.TABLES
+
+select * from INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'fact_sales'
+select * from INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'dim_customers'
+select * from INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'dim_products'
+
+select count(*) from Gold.fact_sales
+
+select count(*) from Gold.dim_customers
+
+select count(*) from Gold.dim_products
+
+-- verifying the customer and the sales table referencing integration:
+select 
+COUNT(*) AS rows_not_connected
+FROM Gold.fact_sales f
+LEFT JOIN Gold.dim_customers c 
+    ON f.customer_key = c.customer_key
+WHERE c.customer_key IS NULL;
+
+-- verifying the customer and the sales table referencing integration:
+select COUNT(*) AS rows_not_connected
+FROM Gold.fact_sales g
+LEFT JOIN Gold.dim_products h 
+    ON g.product_key = h.product_key
+WHERE h.product_key IS NULL;
